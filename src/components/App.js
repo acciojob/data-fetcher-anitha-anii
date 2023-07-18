0e54edfe82b92534947ec1fcbaa9ecfc9c1a8a22
anitha-anii <anithachowdary355@gmail.com>
2023-07-18 18:51:29 +0530

    'changes'

--- src/components/App.js
+++ src/components/App.js
@@ -4,26 +4,34 @@ import "./../styles/App.css";
 const App = () => {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     fetch("https://dummyjson.com/products")
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return res.json();
+      })
       .then((data) => {
         setData(data);
         setLoading(false);
       })
       .catch((error) => {
         console.error("Error fetching data:", error);
+        setError("An error occurred: " + error.message);
         setLoading(false);
       });
   }, []);
 
   return (
     <div>
-      <h1>Data Fetches from API</h1>
-      {loading ? <p>Loading data...</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
+      <h1>Data Fetched from API</h1>
+      {loading ? <p>Loading data...</p> : error ? <p>{error}</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
     </div>
   );
 };
+
 
 export default App;
